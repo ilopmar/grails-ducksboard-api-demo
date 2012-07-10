0c065f323472dbb34384030023219e91bb31bae5
Iván López <lopez.ivan@gmail.com>
2012-07-10 10:32:15 +0200

    Commented out the kickstart filter

--- grails-app/conf/kickstart/KickstartFilters.groovy
+++ grails-app/conf/kickstart/KickstartFilters.groovy
@@ -10,7 +10,7 @@ class KickstartFilters {
         all() {
             before = {
 				// Small "logging" filter for controller & actions
-                println !params.controller ? '/: ' + params : params.controller +"."+(params.action ?: "index")+": "+params
+                //println !params.controller ? '/: ' + params : params.controller +"."+(params.action ?: "index")+": "+params
 				// Better logging: needs to be switched on in Config.groovy and is a little bit verbose
 				//	log.info(!params.controller ? '/: ' + params : params.controller +"."+(params.action ?: "index")+": "+params)
 				// Small "logging" filter for duration of actions: take start time
