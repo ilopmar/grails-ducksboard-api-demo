499eaa4d8e52a043d0cdaaa698f71ae2dc25a802
Iván López <lopez.ivan@gmail.com>
2013-05-29 23:05:26 +0200

    Fix status resources definition

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -3,17 +3,17 @@ modules = {
         dependsOn 'jquery'
         //defaultBundle 'ui'
         
-        resource url='bootstrap/js/bootstrap.js'
-        resource url='datepicker/js/bootstrap-datepicker.js'
-        resource url='kickstart/js/kickstart.js'
-        resource url='js/application.js'
-        resource url='js/kaleidos.notifier.js'
+        resource url:'bootstrap/js/bootstrap.js'
+        resource url:'datepicker/js/bootstrap-datepicker.js'
+        resource url:'kickstart/js/kickstart.js'
+        resource url:'js/application.js'
+        resource url:'js/kaleidos.notifier.js'
         
-        resource url='bootstrap/css/bootstrap.css'
-        resource url='bootstrap/css/bootstrap-responsive.css'
-        resource url='kickstart/css/docs.css'
-        resource url='kickstart/css/kickstart.css'
-        resource url='datepicker/css/datepicker.css'
-        resource url='css/demo.css'
+        resource url:'bootstrap/css/bootstrap.css'
+        resource url:'bootstrap/css/bootstrap-responsive.css'
+        resource url:'kickstart/css/docs.css'
+        resource url:'kickstart/css/kickstart.css'
+        resource url:'datepicker/css/datepicker.css'
+        resource url:'css/demo.css'
     }
 }
